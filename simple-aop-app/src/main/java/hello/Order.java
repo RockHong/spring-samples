14f99cdd59d7b9648622b99d97589344ba687cb5
Hong <hongzhihua@gmail.com>
2015-07-22 13:10:02 +0800

    simple aop app

--- simple-aop-app/src/main/java/hello/Order.java
+++ simple-aop-app/src/main/java/hello/Order.java
@@ -14,12 +14,25 @@ public class Order {
     
     @Auditable
     public double calculateAmount(double amount, double discount) {
-    	Random random = new Random(System.currentTimeMillis());
     	try {
-			Thread.sleep(random.nextLong() % 5000);
+			Thread.sleep(getSleepTime());
 		} catch (InterruptedException e) {
 			LOGGER.info("sleep is interrupted");
 		}
         return amount*discount;
+    }
+    
+    private long getSleepTime() {
+    	//http://stackoverflow.com/questions/5887709/getting-random-numbers-in-java
+    	int MAX = 5000;
+    	
+    	long ret1 = 0;
+    	Random random = new Random(System.currentTimeMillis());
+    	ret1 = random.nextInt(MAX);
+    	
+    	long ret2 = 0;
+    	ret2 = (long)(Math.random() * MAX + 1);
+    	
+    	return ret1;
     }
 }
