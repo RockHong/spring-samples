1659be6dd8017d568b76a6f59398ea2d40894051
Hong <hongzhihua@gmail.com>
2016-06-27 13:11:52 +0800

    add asm sample project

---
+++ asm-example/src/main/java/hello/User.java
@@ -0,0 +1,33 @@
+package hello;
+
+import java.util.List;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private List<String> alias;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String name) {
+        this.lastName = name;
+    }
+
+    public List<String> getAlias() {
+        return alias;
+    }
+
+    public void setAlias(List<String> alias) {
+        this.alias = alias;
+    }
+}
